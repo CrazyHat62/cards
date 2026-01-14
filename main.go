4b11d82df4451541a0ca8e51e3c13c391726c6f6
Richard Porteous <richardporteous343@gmail.com>
2026-01-14 15:09:35 -0700

    moved screens to own functions

--- main.go
+++ main.go
@@ -24,6 +24,9 @@ var cards []Card
 var card Card
 var hand []Card
 
+type game func()
+type display func(rl.Rectangle)
+
 func main() {
 
 	rl.InitWindow(screenW, screenH, "raylib [core] example - basic screen manager")
@@ -31,6 +34,9 @@ func main() {
 	var currentScreen gameScreen
 	currentScreen = LOGO
 	frames := 0
+
+	var gamePlay game = freeCellGame
+	var gameUI display = freeCellUI
 
 	rl.SetTargetFPS(60)
 
@@ -63,48 +69,14 @@ func main() {
 		rec := rl.NewRectangle(0, 0, float32(screenW), float32(screenH))
 		switch currentScreen {
 		case LOGO:
-			txt := "YOUR LOGO GOES HERE"
-			txtlen := rl.MeasureText(txt, 50)
-			rl.DrawText(txt, screenW/2-txtlen/2-3, screenH/2-50+3, 50, rl.Magenta)
-			rl.DrawText(txt, screenW/2-txtlen/2-1, screenH/2-50+1, 50, rl.Black)
-			rl.DrawText(txt, screenW/2-txtlen/2, screenH/2-50, 50, rl.White)
-			txt = "this message disappears in " + fmt.Sprint(240-frames) + " frames"
-			txtlen = rl.MeasureText(txt, 30)
-			rl.DrawText(txt, screenW/2-txtlen/2-3, screenH/2+3, 30, rl.Magenta)
-			rl.DrawText(txt, screenW/2-txtlen/2-1, screenH/2+1, 30, rl.Black)
-			rl.DrawText(txt, screenW/2-txtlen/2, screenH/2, 30, rl.White)
+			splash(frames)
 		case TITLE:
-			rl.DrawRectangleRec(rec, rl.DarkGreen)
-			txt := "AN AMAZING TITLE GOES HERE"
-			txtlen := rl.MeasureText(txt, 50)
-			rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2-50+2, 50, rl.Black)
-			rl.DrawText(txt, screenW/2-txtlen/2, screenH/2-50, 50, rl.White)
-			txt = "press enter to move to next screen"
-			txtlen = rl.MeasureText(txt, 30)
-			rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2+2, 30, rl.Black)
-			rl.DrawText(txt, screenW/2-txtlen/2, screenH/2, 30, rl.White)
-			freeCellGame()
+			titleScreen(rec)
+			gamePlay()
 		case GAMEPLAY:
-			freeCellUI(rec)
-			// rl.DrawRectangleRec(rec, rl.DarkPurple)
-			// txt := "FUN GAMEPLAY GOES HERE"
-			// txtlen := rl.MeasureText(txt, 50)
-			// rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2-50+2, 50, rl.Black)
-			// rl.DrawText(txt, screenW/2-txtlen/2, screenH/2-50, 50, rl.White)
-			// txt = "press enter to move to next screen"
-			// txtlen = rl.MeasureText(txt, 30)
-			// rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2+2, 30, rl.Black)
-			// rl.DrawText(txt, screenW/2-txtlen/2, screenH/2, 30, rl.White)
+			gameUI(rec)
 		case ENDING:
-			rl.DrawRectangleRec(rec, rl.DarkBlue)
-			txt := "A DRAMATIC ENDING GOES HERE"
-			txtlen := rl.MeasureText(txt, 50)
-			rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2-50+2, 50, rl.Black)
-			rl.DrawText(txt, screenW/2-txtlen/2, screenH/2-50, 50, rl.White)
-			txt = "press enter to move to next screen"
-			txtlen = rl.MeasureText(txt, 30)
-			rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2+2, 30, rl.Black)
-			rl.DrawText(txt, screenW/2-txtlen/2, screenH/2, 30, rl.White)
+			endScreen(rec)
 		}
 
 		rl.EndDrawing()
@@ -142,3 +114,40 @@ func freeCellGame() {
 
 // 	rl.DrawRectangleRec(rec, rl.DarkPurple)
 // }
+
+func splash(frames int) {
+	txt := "YOUR LOGO GOES HERE"
+	txtlen := rl.MeasureText(txt, 50)
+	rl.DrawText(txt, screenW/2-txtlen/2-3, screenH/2-50+3, 50, rl.Magenta)
+	rl.DrawText(txt, screenW/2-txtlen/2-1, screenH/2-50+1, 50, rl.Black)
+	rl.DrawText(txt, screenW/2-txtlen/2, screenH/2-50, 50, rl.White)
+	txt = "this message disappears in " + fmt.Sprint(240-frames) + " frames"
+	txtlen = rl.MeasureText(txt, 30)
+	rl.DrawText(txt, screenW/2-txtlen/2-3, screenH/2+3, 30, rl.Magenta)
+	rl.DrawText(txt, screenW/2-txtlen/2-1, screenH/2+1, 30, rl.Black)
+	rl.DrawText(txt, screenW/2-txtlen/2, screenH/2, 30, rl.White)
+}
+
+func titleScreen(rec rl.Rectangle) {
+	rl.DrawRectangleRec(rec, rl.DarkGreen)
+	txt := "AN AMAZING TITLE GOES HERE"
+	txtlen := rl.MeasureText(txt, 50)
+	rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2-50+2, 50, rl.Black)
+	rl.DrawText(txt, screenW/2-txtlen/2, screenH/2-50, 50, rl.White)
+	txt = "press enter to move to next screen"
+	txtlen = rl.MeasureText(txt, 30)
+	rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2+2, 30, rl.Black)
+	rl.DrawText(txt, screenW/2-txtlen/2, screenH/2, 30, rl.White)
+}
+
+func endScreen(rec rl.Rectangle) {
+	rl.DrawRectangleRec(rec, rl.DarkBlue)
+	txt := "A DRAMATIC ENDING GOES HERE"
+	txtlen := rl.MeasureText(txt, 50)
+	rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2-50+2, 50, rl.Black)
+	rl.DrawText(txt, screenW/2-txtlen/2, screenH/2-50, 50, rl.White)
+	txt = "press enter to move to next screen"
+	txtlen = rl.MeasureText(txt, 30)
+	rl.DrawText(txt, screenW/2-txtlen/2-2, screenH/2+2, 30, rl.Black)
+	rl.DrawText(txt, screenW/2-txtlen/2, screenH/2, 30, rl.White)
+}
